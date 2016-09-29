385e15691da794c2c3d05439cd6d32c594b47968
ger <4ndres.gaviria@gmail.com>
2016-09-29 13:44:15 -0300

    first commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/minos/Dropbox/varios1/1empresa/django/project2_virtual_env/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
